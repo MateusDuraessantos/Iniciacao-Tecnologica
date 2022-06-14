769001bb43d67afa80686518be326c7a150b459f
MateusDuraessantos <mateusduraessantos@gmail.com>
2022-06-14 01:24:18 -0300

    Interação de navegação e implementação do vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
     <NewHeader />
-    <ListaBraba />
-    <MainContent />
     <NewFooter />
   </div>
 </template>
 
 <script>
-import ListaBraba from "./components/Lista.vue";
-import MainContent from "./components/Main.vue";
 import NewHeader from "./components/NewHeader.vue";
 import NewFooter from "./components/NewFooter.vue";
 
